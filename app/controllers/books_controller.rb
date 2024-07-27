dde8f4f424217ae224017de7b9ba292d9eca7b88
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-07-27 15:28:38 +0000

    [finish]booker2-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update] 
   def index
     @book = Book.new                            #@book = Bookモデルの新しいインスタンス
     @user = User.all
@@ -18,15 +19,41 @@ class BooksController < ApplicationController
       render 'index'                            #取得した情報を元にbookのindex画面を表示します
     end
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
 
   def show
+    @newbook = Book.new
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def book_params
     #送られてきたデータがbookテーブルのtitleとbodyであるかの判断
     params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get 'about', to:'homes#about', as:'about'
+  get 'home/about', to:'homes#about', as:'about'
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:show, :index, :edit, :create, :destroy, :update,]
 end
