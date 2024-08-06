25bcb868f8e63b6603653a7982786d66d397a598
EC2 Default User <ec2-user@ip-172-31-1-163.ap-northeast-1.compute.internal>
2024-08-06 16:02:09 +0000

    [slick]

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,17 +2,37 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+// = require_tree .
+//= require jquery
+//= require jquery_ujs
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import $ from 'jquery';
+import "slick-carousel";
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
 
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("trix")
+require("@rails/actiontext")
+
+document.addEventListener("DOMContentLoaded", () => {
+  $('.your-class').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  });
+});
